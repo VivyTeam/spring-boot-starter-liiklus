2fd1052af276f897e7e788ccf98da9068393fb4f
Kirill Merkushev <lanwen@yandex.ru>
2019-07-25 15:56:30 +0200

    add event processor interface to be more hight-level

---
+++ starter/src/main/java/io/vivy/liiklus/event/EventLogProcessor.java
@@ -0,0 +1,22 @@
+package io.vivy.liiklus.event;
+
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.function.Function;
+
+public interface EventLogProcessor<T> extends Function<EventLogProcessor.Event<T>, Mono<?>> {
+
+    interface Event<T> {
+
+        long getOffset();
+
+        String getKey();
+
+        T getValue();
+
+        Instant getTimestamp();
+
+        boolean isReplay();
+    }
+}
