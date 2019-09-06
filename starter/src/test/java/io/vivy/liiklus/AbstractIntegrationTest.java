289979de2c75c9e15dff2affb9a88281d2d1c6a2
Kirill Merkushev <lanwen@yandex.ru>
2019-09-06 17:02:42 +0200

    move boot annotations to tests, mock web where not needed

--- starter/src/test/java/io/vivy/liiklus/AbstractIntegrationTest.java
+++ starter/src/test/java/io/vivy/liiklus/AbstractIntegrationTest.java
@@ -4,19 +4,13 @@ import com.github.bsideup.liiklus.LiiklusClient;
 import com.github.bsideup.liiklus.container.LiiklusContainer;
 import com.github.bsideup.liiklus.protocol.GetOffsetsRequest;
 import com.github.bsideup.liiklus.protocol.PublishReply;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
+import io.vivy.liiklus.support.LoggingRecordProcessor;
 import org.assertj.core.api.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.GenericContainer;
 
 import java.time.Duration;
@@ -27,22 +21,13 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(
-        classes = {AbstractIntegrationTest.WithRecordProcessor.class},
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {
-                "liiklus.groupName=${random.uuid}-consumer",
-        }
-)
 public class AbstractIntegrationTest {
 
     static {
-        LiiklusContainer liiklus = new LiiklusContainer("0.8.2")
-                .withExposedPorts(8081)
-                .withEnv("storage_records_type", "MEMORY");
+        LiiklusContainer liiklus = new LiiklusContainer("0.9.0")
+                .withExposedPorts(8081);
 
-        Stream.of(liiklus).parallel().forEach(GenericContainer::start);
+        liiklus.start();
 
         System.getProperties().putAll(Map.of(
                 "liiklus.target", "rsocket://" + liiklus.getContainerIpAddress() + ":" + liiklus.getMappedPort(8081),
@@ -50,17 +35,6 @@ public class AbstractIntegrationTest {
                 "liiklus.groupVersion", "1",
                 "liiklus.ackInterval", "10ms"
         ));
-    }
-
-    @Configuration
-    @Import(TestApplication.class)
-    public static class WithRecordProcessor {
-
-        @Bean
-        public LoggingRecordProcessor loggingRecordProcessor() {
-            return new LoggingRecordProcessor();
-        }
-
     }
 
     @Autowired
@@ -71,19 +45,6 @@ public class AbstractIntegrationTest {
 
     @Autowired
     protected LiiklusProperties liiklusProperties;
-
-    @LocalServerPort
-    private int port;
-
-    protected RequestSpecification requestSpecification;
-
-    @BeforeEach
-    void setUpRequestSpecification() {
-        requestSpecification = new RequestSpecBuilder()
-                .setContentType(ContentType.JSON)
-                .setPort(port)
-                .build();
-    }
 
     protected void waitForLiiklusOffset(PublishReply latestOffset) {
         await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
