21c5606af7394d4ebc605eca5c53cce533e9f2c1
Kirill Merkushev <lanwen@yandex.ru>
2020-06-25 18:00:17 +0200

    test partition handler precisely

---
+++ starter/src/test/java/io/vivy/liiklus/consumer/ConsumerTest.java
@@ -0,0 +1,82 @@
+package io.vivy.liiklus.consumer;
+
+import com.github.bsideup.liiklus.protocol.ReceiveReply;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+class ConsumerTest {
+
+    @Test
+    @Timeout(5)
+    void shouldPassOnlySamplesToAck() {
+        StepVerifier.withVirtualTime(
+                () -> Flux.interval(Duration.ofSeconds(1))
+                        .map(i -> ReceiveReply.Record.newBuilder().setOffset(i).build())
+                        .log("records")
+                        .transformDeferred(
+                                records -> new LiiklusConsumerFactory.PartitionHandler(
+                                        new LiiklusConsumer() {
+                                            @Override
+                                            public Mono<Void> consume(int partition, ReceiveReply.Record record) {
+                                                return Mono.empty();
+                                            }
+                                        },
+                                        Schedulers.parallel(),
+                                        Duration.ofSeconds(5)
+                                ).apply(0, records)
+                        )
+                        .take(2)
+                        .log("flow"),
+                1
+        )
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(10))
+                .expectNextCount(1)
+                .expectNoEvent(Duration.ofMinutes(1))
+                .thenRequest(1)
+                .thenAwait(Duration.ofSeconds(5))
+                .expectNextCount(1)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    @Timeout(5)
+    void shouldProduceLackRequestsError() {
+        StepVerifier.withVirtualTime(() -> Flux
+                        .interval(Duration.ofSeconds(1)).log("interval")
+                        .sample(Duration.ofSeconds(5)).log("sample")
+                        .take(2)
+                , 1)
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(10))
+                .expectNextCount(1)
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    @Timeout(5)
+    void shouldNotProduceLackRequestsError() {
+        StepVerifier.withVirtualTime(() -> Flux
+                        .interval(Duration.ofSeconds(1)).log("interval")
+                        .sample(Duration.ofSeconds(5)).log("sample")
+                        .onBackpressureLatest().log("backpressure")
+                        .take(2)
+                , 1)
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(10))
+                .expectNext(3L)
+                .thenRequest(1)
+                .thenAwait(Duration.ofSeconds(10))
+                .expectNext(8L)
+                .expectComplete()
+                .verify();
+    }
+}
