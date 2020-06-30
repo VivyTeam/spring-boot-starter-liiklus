64152209fd0382947e0c7df411af21dab97ff465
Kirill Merkushev <kirill@vivy.com>
2020-06-30 16:26:53 +0200

    test partition handler precisely (#32)
    * test partition handler precisely
    
    * cancel at the right moment

--- starter/src/main/java/io/vivy/liiklus/consumer/LiiklusConsumerFactory.java
+++ starter/src/main/java/io/vivy/liiklus/consumer/LiiklusConsumerFactory.java
@@ -1,17 +1,22 @@
 package io.vivy.liiklus.consumer;
 
 import com.github.bsideup.liiklus.LiiklusClient;
+import com.github.bsideup.liiklus.protocol.ReceiveReply;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.experimental.FieldDefaults;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.ReplayProcessor;
 import reactor.core.publisher.SignalType;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
+import java.util.function.BiFunction;
 import java.util.logging.Level;
 
 @RequiredArgsConstructor
@@ -32,25 +37,34 @@ public class LiiklusConsumerFactory {
                 readScheduler,
                 ackScheduler,
                 liiklusClient,
-                (partition, records) -> {
-                    var ackInProgress = ReplayProcessor.cacheLastOrDefault(false);
-                    var ackFinished = ackInProgress.filter(Boolean.FALSE::equals);
-
-                    return records
-                            .concatMap(
-                                    record -> Mono.defer(() -> liiklusConsumer.consume(partition, record))
-                                            .log("processor", Level.SEVERE, SignalType.ON_ERROR)
-                                            .retryWhen(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(1)))
-                                            .delaySubscription(ackFinished)
-                                            .thenReturn(record.getOffset()),
-                                    1_000
-                            )
-                            .sample(Flux.interval(ackInterval, ackScheduler))
-                            .onBackpressureLatest()
-                            .doOnNext(__ -> ackInProgress.onNext(true))
-                            .doOnRequest(__ -> Schedulers.parallel().schedule(() -> ackInProgress.onNext(false)));
-                }
+                new PartitionHandler(liiklusConsumer, ackScheduler, ackInterval)
         );
     }
 
+    @Value
+    static class PartitionHandler implements BiFunction<Integer, Flux<ReceiveReply.Record>, Publisher<Long>> {
+        LiiklusConsumer liiklusConsumer;
+        Scheduler ackScheduler;
+        Duration ackInterval;
+
+        @Override
+        public Publisher<Long> apply(Integer partition, Flux<ReceiveReply.Record> records) {
+            var ackInProgress = ReplayProcessor.cacheLastOrDefault(false);
+            var ackFinished = ackInProgress.filter(Boolean.FALSE::equals);
+
+            return records
+                    .concatMap(
+                            record -> Mono.defer(() -> liiklusConsumer.consume(partition, record))
+                                    .log("processor", Level.SEVERE, SignalType.ON_ERROR)
+                                    .retryWhen(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(1)))
+                                    .delaySubscription(ackFinished)
+                                    .thenReturn(record.getOffset()),
+                            1_000
+                    )
+                    .sample(Flux.interval(ackInterval, ackScheduler))
+                    .onBackpressureLatest()
+                    .doOnNext(__ -> ackInProgress.onNext(true))
+                    .doOnRequest(__ -> Schedulers.parallel().schedule(() -> ackInProgress.onNext(false)));
+        }
+    }
 }
